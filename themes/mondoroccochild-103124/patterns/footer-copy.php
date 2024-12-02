c61d99fda52cff343af065a5cef81ecc1f0a63f8
Anne S. Katzeff <anne@askdesign.biz>
2024-12-02 18:33:30 -0500

    Clean up template parts
    • Duplicate and rename template parts that are being used
    • Delete unused template parts
    • Update all templates with correctly-named template parts
    • Save changes to theme (do not include synced patterns, which had already been included and were thus unsync'd!)
    • Export newly updated child theme as zip archive

---
+++ themes/mondoroccochild-103124/patterns/footer-copy.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Title: footer-copy
+ * Slug: mondoroccochild/footer-copy
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"0px","right":"0px"}},"border":{"top":{"color":"var:preset|color|custom-accent-dark-1","width":"6px"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}}},"backgroundColor":"custom-header","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group has-custom-header-background-color has-background" style="border-top-color:var(--wp--preset--color--custom-accent-dark-1);border-top-width:6px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;padding-top:var(--wp--preset--spacing--10);padding-right:0px;padding-bottom:var(--wp--preset--spacing--10);padding-left:0px"><!-- wp:site-logo /-->
+
+<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","justifyContent":"center"},"fontSize":"small"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"typography":{"lineHeight":"1"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);line-height:1"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"0.8rem"}}} -->
+<p class="has-text-align-center" style="font-size:0.8rem"><strong>© Copyright Rocco Giuliano. All rights reserved.</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|10","left":"1rem"}},"typography":{"fontSize":"0.8rem"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--10);padding-left:1rem;font-size:0.8rem"><!-- wp:paragraph -->
+<p><strong>Rocco Giuliano</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"width":"15px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/mondoRocco.bowtie-2024-gold.png" alt="Mondo Rocco bowtie" class="" style="width:15px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><strong>Studio 34</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"width":"15px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/mondoRocco.bowtie-2024-gold.png" alt="Mondo Rocco bowtie" class="" style="width:15px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><strong>249 A Street</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"width":"15px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/mondoRocco.bowtie-2024-gold.png" alt="Mondo Rocco bowtie" class="" style="width:15px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><strong>Boston, MA 02210</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|10","left":"1rem"}},"typography":{"fontSize":"0.8rem"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--10);padding-left:1rem;font-size:0.8rem"><!-- wp:paragraph -->
+<p><strong>617.426.6135</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"width":"15px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/mondoRocco.bowtie-2024-gold.png" alt="Mondo Rocco bowtie" class="" style="width:15px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><strong>rocco@mondorocco.com</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
